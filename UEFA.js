28e31b8e3904406eca9db2c5599f924cfba8b196
RizkyV <juulviktor@gmail.com>
2023-06-14 12:13:16 +0200

    real fixtures now

--- UEFA.js
+++ UEFA.js
@@ -1,6 +1,6 @@
 function getLeagues() {
     var text = '';
-    fetcher('fixtures?season=2022&league=848', (result) => {
+    fetcher('fixtures?season=2022&league=3', (result) => {
 
         var a = document.getElementById('download');
         var file = new Blob([JSON.stringify(result.response)], { type: 'json' });
@@ -117,50 +117,131 @@ function readLocal() {
                     })
                     console.log(result);
 
+                    fetch("https://raw.githubusercontent.com/RizkyV/UEFA/main/data/fixtures-season=2022-league=3.json")
+                        .then((res) => res.text())
+                        .then((result) => {
+                            var result = JSON.parse(result);
+                            result.forEach((fixture) => {
+                                fixtures.push(fixture);
+                            })
+                            console.log(result);
+
+                            fetch("https://raw.githubusercontent.com/RizkyV/UEFA/main/data/fixtures-season=2022-league=848.json")
+                                .then((res) => res.text())
+                                .then((result) => {
+                                    var result = JSON.parse(result);
+                                    result.forEach((fixture) => {
+                                        fixtures.push(fixture);
+                                    })
+                                    console.log(result);
+
+                                    seasons.find(element => element.season = '2022').fixtures = fixtures;
+                                    calculateCoefficients();
+                                })
+                                .catch((e) => console.error(e));
+
+                        })
+                        .catch((e) => console.error(e));
+
                 })
                 .catch((e) => console.error(e));
 
-            fetch("https://raw.githubusercontent.com/RizkyV/UEFA/main/data/fixtures-season=2022-league=3.json")
-                .then((res) => res.text())
-                .then((result) => {
-                    var result = JSON.parse(result);
-                    result.forEach((fixture) => {
-                        fixtures.push(fixture);
-                    })
-                    console.log(result);
 
-                })
-                .catch((e) => console.error(e));
 
-            fetch("https://raw.githubusercontent.com/RizkyV/UEFA/main/data/fixtures-season=2022-league=848.json")
-                .then((res) => res.text())
-                .then((result) => {
-                    var result = JSON.parse(result);
-                    result.forEach((fixture) => {
-                        fixtures.push(fixture);
-                    })
-                    console.log(result);
-                })
-                .catch((e) => console.error(e));
+
         })
         .catch((e) => console.error(e));
 
 
 }
 
-function calculateCoefficient() {
+function calculateCoefficients() {
     //@todo instead of one long fixture array, separate into 3 by league
     console.log('calculating');
-
+    console.log(teams);
     teams.forEach((team) => {
+        if (team.team.id !== 40) {
+            return;
+        }
+        var clubPoints = 0;
         console.log(team);
 
-        fixtures.forEach(() => {
+        seasons.forEach((season) => {
+            console.log(season);
+            var clubPointsSeason = 0;
+            var countryPointsSeason = 0;
+            var bonuses = [{ 'CLGS': false }, { 'ELGS': false }];
+            season.fixtures.forEach((fixture) => {
+                console.log(fixture.league.name);
+                console.log(fixture.league.round);
+                console.log(fixture);
+                clubPointsSeason += ptsForFixture(team.team, fixture, bonuses);
+                console.log(clubPointsSeason);
+            });
 
 
+            if (bonuses.CLGS) {
+                clubPointsSeason += 4;
+            }
 
-        })
+            console.log('clubPointsSeason');
+            console.log(clubPointsSeason);
+            clubPoints += clubPointsSeason;
+        });
+        console.log('clubPoints');
+        console.log(clubPoints);
     })
+}
+
+function ptsForFixture(team, fixture, bonuses) {
+    var CL = 'UEFA Champions League';
+    var ECL = 'UEFA Europa Conference League';
+
+    var pts = 0;
+    if (isParticipant(team, fixture)) {
+        console.log('participated');
+        if (fixture.league.name === CL) {
+            if (isHome(team, fixture)) {
+                if (fixture.teams.home.winner) {
+                    console.log('won');
+                    pts += 2;
+                } else if (fixture.teams.home.winner === null) {
+                    console.log('drew');
+                    pts += 1;
+                }
+            } else {
+                if (fixture.teams.away.winner) {
+                    console.log('won');
+                    pts += 2;
+                } else if (fixture.teams.away.winner === null) {
+                    console.log('drew');
+                    pts += 1;
+                }
+            }
+        }
+        console.log(pts);
+    }
+
+    return pts;
+}
+
+
+function isParticipant(team, fixture) {
+    if (fixture.teams.home.id === team.id) {
+        return true;
+    } else if (fixture.teams.away.id === team.id) {
+        return true;
+    }
+
+    return false;
+}
+
+function isHome(team, fixture) {
+    if (fixture.teams.home.id === team.id) {
+        return true;
+    } else if (fixture.teams.away.id === team.id) {
+        return false;
+    }
 }
 
 var countries = [];
@@ -168,6 +249,8 @@ var countryRankings = [];
 var teams = [];
 var fixtures = [];
 readLocal();
+var seasons = [{ 'season': '2022', 'fixtures': [] }, { 'season': '2021', 'fixtures': [] }, { 'season': '2020', 'fixtures': [] }, { 'season': '2019', 'fixtures': [] }, { 'season': '2018', 'fixtures': [] }]
+calculateCoefficients();
 
 /**
  * TO DO:
